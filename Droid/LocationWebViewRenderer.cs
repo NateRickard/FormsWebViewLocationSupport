05de7e5e0e21c447db6a053b58421166cab80122
Nate Rickard <nate.rickard@xamarin.com>
2016-04-14 18:21:57 -0400

    initial commit

---
+++ Droid/LocationWebViewRenderer.cs
@@ -0,0 +1,58 @@
+﻿using Android;
+using Android.App;
+using Android.Content;
+using Android.Webkit;
+using FormsWebViewLocationSupport.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: UsesPermission (Name = Manifest.Permission.AccessFineLocation)]
+[assembly: UsesPermission (Name = Manifest.Permission.Internet)]
+
+[assembly: ExportRenderer (typeof (Xamarin.Forms.WebView), typeof (LocationWebViewRenderer))]
+
+namespace FormsWebViewLocationSupport.Droid
+{
+	public class LocationWebViewRenderer : WebViewRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<Xamarin.Forms.WebView> e)
+		{
+			base.OnElementChanged (e);
+
+			//initial setup
+			if (e.OldElement == null) {
+				Control.Settings.SetGeolocationEnabled (true);
+			}
+		}
+
+		protected override FormsWebChromeClient GetFormsWebChromeClient ()
+		{
+			return new MyWebChromeClient (base.Context);
+		}
+	}
+
+
+	public class MyWebChromeClient : FormsWebChromeClient
+	{
+		private readonly Context _context;
+
+		public MyWebChromeClient (Context context)
+		{
+			_context = context;
+		}
+
+		public override void OnGeolocationPermissionsShowPrompt (string origin, GeolocationPermissions.ICallback callback)
+		{
+			const bool remember = false;
+			var builder = new AlertDialog.Builder (_context);
+
+			builder.SetTitle ("Location")
+				.SetMessage (string.Format ("{0} would like to use your current location", origin))
+				.SetPositiveButton ("Allow", (sender, args) => callback.Invoke (origin, true, remember))
+				.SetNegativeButton ("Disallow", (sender, args) => callback.Invoke (origin, false, remember));
+
+			var alert = builder.Create ();
+			alert.Show ();
+		}
+	}
+}
---
+++ FormsWebViewLocationSupport/FormsWebViewLocationSupport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FormsWebViewLocationSupport
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			var webView = new WebView ();
+			webView.HeightRequest = 500;
+			webView.Source = "https://maps.google.com";
+
+			//iOS: see http://stackoverflow.com/questions/26631417/allowing-location-access-inside-uiwebview
+			//	You must set the NSLocationWhenInUseUsageDescription key in info.plist in order to get prompted.
+
+			//Android: We've created a custom renderer which can be found in the Android project
+
+			// The root page of your application
+			var content = new ContentPage {
+				Title = "Forms WebView Location Support",
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.StartAndExpand,
+					Children = {
+						webView
+					}
+				}
+			};
+
+			MainPage = new NavigationPage (content);
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
